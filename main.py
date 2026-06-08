1c6f945fdcacbc603459159f867e8c0fabb75162
Sajeer <sajeerfm93@gmail.com>
2026-06-08 22:16:32 +0530

    added splitter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from rag.loader import load_pdf
+from rag.splitter import split_documents
 
 docs = load_pdf("data/sample.pdf")
 
-print(docs[0].page_content[:500])
+chunk = split_documents(docs)
+
+print(len(chunk))
+print(chunk[3].page_content)
