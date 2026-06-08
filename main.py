ddadd73518f4201441b676ae1d05b8910fbe3683
Sajeer <sajeerfm93@gmail.com>
2026-06-09 00:03:28 +0530

    api connections

--- main.py
+++ main.py
@@ -1,28 +1,55 @@
-from rag.loader import load_pdf
-from rag.splitter import split_documents
-from rag.embedder import get_embedding_model
-from rag.vector_store import load_vector_store
-from rag.chatbot import ask_rag
+# from rag.loader import load_pdf
+# from rag.splitter import split_documents
+# from rag.embedder import get_embedding_model
+# from rag.vector_store import load_vector_store
+# from rag.chatbot import ask_rag
 
-docs = load_pdf("data/sample.pdf")
+# docs = load_pdf("data/sample.pdf")
 
-chunks = split_documents(docs)
+# chunks = split_documents(docs)
 
-embeddings = get_embedding_model()
+# embeddings = get_embedding_model()
 
-db = load_vector_store(embeddings)
+# db = load_vector_store(embeddings)
 
 
-while True:
-    query = input("\nAsk Question: ")
+# while True:
+#     query = input("\nAsk Question: ")
 
-    if query.lower() == "exit":
-        break
+#     if query.lower() == "exit":
+#         break
 
-    answer = ask_rag(
-        query,
-        db
-    )
+#     answer = ask_rag(
+#         query,
+#         db
+#     )
 
-    print("\nAnswer:")
-    print(answer)
+#     print("\nAnswer:")
+#     print(answer)
+
+from fastapi import FastAPI,UploadFile,File
+
+app = FastAPI(
+    title = "StudyMate",
+    version = "1.0.0"
+)
+
+@app.get("/")
+def home():
+    return {
+        "message":"StudyMate Running"
+    }
+
+@app.post("/upload")
+async def upload_pdf(
+    file: UploadFile = File(...)
+):
+    filepath = f"data/{file.filename}"
+
+    with open(filepath,"wb") as f:
+        f.write(await file.read())
+
+    return {
+        "message":"PDF Uploaded",
+        "filename":file.filename
+    }
--- rag/vector_store.py
+++ rag/vector_store.py
@@ -1,11 +1,13 @@
 from langchain_chroma import Chroma
 
-# def create_vector_store(chunks, embeddings):
-#     vectordb = Chroma.from_documents(
-#         documents = chunks,
-#         embedding = embeddings,
-#         persist_directory = "chroma_db"
-#     )
+def create_vector_store(chunks, embeddings):
+    vectordb = Chroma.from_documents(
+        documents = chunks,
+        embedding = embeddings,
+        persist_directory = "chroma_db"
+    )
+
+    return vectordb
 
 def load_vector_store(embeddings):
     vectordb = Chroma(
