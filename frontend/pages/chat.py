6c2fcede0264aaaa2521e18a5657acf54fc324cd
Sajeer <sajeerfm93@gmail.com>
2026-06-10 13:28:40 +0530

    chat window updated

--- frontend/pages/chat.py
+++ frontend/pages/chat.py
@@ -1,30 +1,115 @@
 import streamlit as st
 import requests
 
-st.title("ChatBot")
+# st.title("ChatBot")
 
-question = st.text_input("Ask a Question")
+# question = st.text_input("Ask a Question")
 
-if st.button("Ask"):
+# if st.button("Ask"):
+#     response = requests.post(
+#         "http://localhost:8000/chat",
+#         json={
+#             "question": question
+#         }
+#     )
+
+#     st.write(response.json()["answer"])
+
+st.set_page_config(
+    page_title="StudyMate",
+    layout="wide"
+)
+
+st.title("StudyMate")
+
+#sidebar
+st.sidebar.title("Documents")
+
+uploaded_file = st.sidebar.file_uploader(
+    "Upload PDF",
+    type=["pdf"]
+)
+
+if uploaded_file:
+
+    if st.sidebar.button("Upload"):
+        files = {
+            "file":(
+                uploaded_file.name,
+                uploaded_file,
+                "application/pdf"
+            )
+        }
+
+        with st.sidebar:
+            with st.spinner("Processing PDF..."):
+                response = requests.post(
+                    "http://localhost:8000/upload",
+                    files=files
+                )
+
+        st.sidebar.success(
+            response.json()["message"]
+        )
+        st.session_state["upload_success"] = True
+        st.rerun()
+
+#Document list in sidebar
+st.sidebar.markdown("---")
+st.sidebar.subheader("Uploaded Documents")
+
+try:
+    docs = requests.get(
+        "http://localhost:8000/files"
+    ).json()
+
+    for doc in docs:
+        col1, col2 = st.sidebar.columns([4, 1])
+
+        with col1:
+            st.write(doc["name"])
+        with col2:
+            if st.button("🗑️",key=doc["file_id"]):
+                requests.delete(
+                    f"http://localhost:8000/pdf/{doc['file_id']}"
+                )
+                st.rerun()
+# except:
+#     st.sidebar.info("No documents uploaded")
+except Exception as e:
+    st.sidebar.error(str(e))
+
+
+#chat section
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
+
+question = st.chat_input("Ask a question...")
+
+if question:
+    st.session_state.messages.append({
+        "role": "user",
+        "content": question
+    })
+
+    with st.chat_message("user"):
+        st.write(question)
+
     response = requests.post(
         "http://localhost:8000/chat",
-        json={
-            "question": question
-        }
+        json={"question": question}
     )
 
-    st.write(response.json()["answer"])
+    answer = response.json()["answer"]
 
-# st.set_page_config(
-#     page_title="StudyMate",
-#     layout="wide"
-# )
+    st.session_state.messages.append({
+        "role": "assistant",
+        "content": answer
+    })
 
-# st.title("StudyMate")
-
-# st.sidebar.tile("Documents")
-
-# uploaded_file = st.sidebar.file_uploader(
-#     "Upload PDF",
-#     type=["pdf"]
-# )
+    with st.chat_message("assistant"):
+        st.write(answer)
